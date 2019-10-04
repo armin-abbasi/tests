b698b4e32438ea46d19b28c19348a330e7bb53b4
Armin Abbasi <armin.abbasi@hotmail.com>
2019-10-04 14:10:07 +0330

    completed Fight class tests

--- Fight.php
+++ Fight.php
@@ -1,9 +1,27 @@
-
 <?php
+
+namespace Test;
 
 require('./vendor/autoload.php');
 
 use PHPUnit\Framework\TestCase;
+
+/**
+ * Override mt_rand() natural behaviour in order
+ * to get idempotent output with each input values.
+ * @param int $min
+ * @param int|NULL $max
+ * @return float
+ */
+function mt_rand(int $min = 2, int $max = NULL): float
+{
+    $min = intval($min);
+    $max = intval($max);
+
+    $max = $max < $min ? $min : $max;
+
+    return round(($max - $min) / 2) + $min;
+}
 
 interface HeroInterface
 {
@@ -24,8 +42,11 @@ class DamageCalculator
     {
         $damage = 0;
 
-        if ($attacker->getAttack() > $defender->getDefence()) {
-            $baseDamage = $attacker->getAttack() - $defender->getDefence();
+        $attackUnits = $attacker->getAttack();
+        $defendUnits = $defender->getDefence();
+
+        if ($attackUnits > $defendUnits) {
+            $baseDamage = $attackUnits - $defendUnits;
 
             $factor = $baseDamage * self::DAMAGE_RAND_FACTOR;
 
@@ -49,10 +70,42 @@ class FightService
     }
 }
 
-class FightServiceTest extends TestCase {
+class FightServiceTest extends TestCase
+{
+    const ATTACK = 15;
+    const DEFENSE = 8;
+    const HEALTH_POINTS = 100;
+
+    private function getDamage(HeroInterface $attacker, HeroInterface $defender)
+    {
+        return DamageCalculator::calculateDamage($attacker, $defender);
+    }
 
     public function testFight()
     {
-        $this->assertTrue(true,true);
+        $fightService = new FightService();
+
+        $attacker = $this->getMockBuilder(HeroInterface::class)->getMock();
+        $defender = $this->getMockBuilder(HeroInterface::class)->getMock();
+
+        $attacker->expects($this->any())
+            ->method('getAttack')
+            ->will($this->returnValue(self::ATTACK));
+
+        $defender->expects($this->any())
+            ->method('getDefence')
+            ->will($this->returnValue(self::DEFENSE));
+
+        $damage = $this->getDamage($attacker, $defender);
+
+        $defender->expects($this->once())
+            ->method('getHealthPoints')
+            ->will($this->returnValue(self::HEALTH_POINTS));
+
+        $defender->expects($this->once())
+            ->method('setHealthPoints')
+            ->with(self::HEALTH_POINTS - $damage);
+
+        $fightService->fight($attacker, $defender);
     }
 }
